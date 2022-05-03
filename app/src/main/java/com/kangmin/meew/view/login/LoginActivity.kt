3076ccb3debc1fb62b794201b2691837e4f7f20b
kangmin1012 <kangmin1012@naver.com>
2022-05-03 10:48:31 +0900

    캐릭터 카드 레이아웃 작성

--- app/src/main/java/com/kangmin/meew/view/login/LoginActivity.kt
+++ app/src/main/java/com/kangmin/meew/view/login/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.kangmin.meew.view.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.kangmin.base.BaseActivity
 import com.kangmin.meew.databinding.ActivityLoginBinding
 import com.kangmin.meew.R
+import com.kangmin.meew.view.signup.SignUpActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,6 +28,14 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
         viewModel.toastMsg.observe(this) {
             it?.let { msg ->
                 Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        viewModel.loginSuccess.observe(this) {
+            it?.let { isSuccess ->
+                if (isSuccess) {
+                    startActivity(Intent(this, SignUpActivity::class.java))
+                }
             }
         }
     }
--- app/src/main/java/com/kangmin/meew/view/login/LoginViewModel.kt
+++ app/src/main/java/com/kangmin/meew/view/login/LoginViewModel.kt
@@ -1,11 +1,14 @@
 package com.kangmin.meew.view.login
 
 import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.data.helper.KakaoLoginHelper
 import com.example.domain.listener.LoginListener
 import com.example.domain.usecase.LoginUseCase
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kangmin.base.BaseViewModel
+import com.kangmin.meew.MeewApplication
 import com.kangmin.meew.util.Dlog
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -16,16 +19,24 @@ class LoginViewModel @Inject constructor(
     private val kakaoLogin: KakaoLoginHelper
 ) : BaseViewModel() {
 
+    private val _loginSuccess = MutableLiveData(false)
+    val loginSuccess: LiveData<Boolean> = _loginSuccess
+
     init {
         _toastMsg.postValue(loginUseCase.getTest())
     }
 
     fun loginKakao(context: Context) {
+        if (MeewApplication.DEBUG) {
+            _loginSuccess.postValue(true)
+            return
+        }
+
         kakaoLogin.loginKakao(
             context,
             object : LoginListener {
             override fun onSuccess() {
-                Dlog.i("KakaoLoginSuccess")
+                _loginSuccess.postValue(true)
             }
 
             override fun onFailed(throwable: Throwable?) {
