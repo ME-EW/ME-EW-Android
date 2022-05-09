410f94bcd3a97e2f58b115d5bf7f759739efbd4d
kangmin1012 <kangmin1012@naver.com>
2022-05-09 11:10:55 +0900

    캐릭터 EnumSet 추가

---
+++ domain/src/main/java/com/example/domain/enum/CharacterEnum.kt
@@ -0,0 +1,12 @@
+package com.example.domain.enum
+
+enum class CharacterEnum(val code: Int) {
+    WIND(1),
+    SUN(2),
+    SKY(3),
+    HONESTY(4),
+    FLOWER(5),
+    CLOUD(6),
+    DIRECTION(7),
+    MIND(8)
+}
