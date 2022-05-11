fd2b933e26bb6e987d2d9552731e846fb36ab132
kangmin1012 <kangmin1012@naver.com>
2022-05-11 17:36:57 +0900

    메인 화면 바텀시트 추가

---
+++ app/src/main/java/com/kangmin/meew/view/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kangmin.meew.view.main
+
+import android.os.Bundle
+import com.kangmin.base.BaseActivity
+import com.kangmin.meew.R
+import com.kangmin.meew.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
