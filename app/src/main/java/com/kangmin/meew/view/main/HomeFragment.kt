1982b9c6ae95de40dd8959b118361313f7d0ac71
kangmin1012 <kangmin1012@naver.com>
2022-05-12 18:10:11 +0900

    홈 화면 상단부 xml 작성

---
+++ app/src/main/java/com/kangmin/meew/view/main/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.kangmin.meew.view.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kangmin.meew.R
+
+
+class HomeFragment : Fragment() {
+
+}
