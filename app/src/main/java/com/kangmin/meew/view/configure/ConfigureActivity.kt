a7cd0e21c84b49f697de45a137190f99a9c9d614
kangmin1012 <kangmin1012@naver.com>
2022-06-14 17:03:12 +0900

    설정 페이지 activity 추가

---
+++ app/src/main/java/com/kangmin/meew/view/configure/ConfigureActivity.kt
@@ -0,0 +1,13 @@
+package com.kangmin.meew.view.configure
+
+import android.os.Bundle
+import com.kangmin.base.BaseActivity
+import com.kangmin.meew.R
+import com.kangmin.meew.databinding.ActivityConfigureBinding
+
+class ConfigureActivity : BaseActivity<ActivityConfigureBinding>(R.layout.activity_configure) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
