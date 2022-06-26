83b6c084edb757ea84f9ad8f4fe6a3da7201f6f5
kangmin1012 <kangmin1012@naver.com>
2022-06-26 17:01:52 +0900

    체크박스 캐릭터마다 다르게 적용하기 위한 bindingadapter

--- app/src/main/java/com/kangmin/meew/view/binding/BindingAdapter.kt
+++ app/src/main/java/com/kangmin/meew/view/binding/BindingAdapter.kt
@@ -6,9 +6,10 @@ import androidx.databinding.BindingAdapter
 import com.example.domain.enum.CharacterEnum
 import com.example.domain.model.CharacterInfo
 import com.kangmin.meew.R
+import com.kangmin.meew.character.CharacterDetailEnum
 
-@BindingAdapter("app:characterCheckBox")
-fun CheckBox.setCharacterCheckBox(character: CharacterInfo) {
+@BindingAdapter("app:roundCheckBox")
+fun CheckBox.setRoundCheckBox(character: CharacterInfo) {
     this.buttonDrawable =
         ContextCompat.getDrawable(
             this.context,
@@ -49,4 +50,46 @@ fun CheckBox.setCharacterCheckBox(character: CharacterInfo) {
                 }
             }
         )
+}
+
+@BindingAdapter("app:squareCheckBox")
+fun CheckBox.setSquareCheckBox(id: Int) {
+    val drawable =
+        ContextCompat.getDrawable(
+            this.context,
+            when(id) {
+                CharacterDetailEnum.BARAM.id -> {
+                    R.drawable.selector_square_check_purple_24dp
+                }
+                CharacterDetailEnum.TAEYANG.id -> {
+                    R.drawable.selector_square_check_red_24dp
+                }
+                CharacterDetailEnum.HANEUL.id -> {
+                    R.drawable.selector_square_check_blue_24dp
+                }
+                CharacterDetailEnum.JUNGJIK.id -> {
+                    R.drawable.selector_square_check_pink_24dp
+                }
+                CharacterDetailEnum.KOTNIM.id -> {
+                    R.drawable.selector_square_check_orange_24dp
+                }
+                CharacterDetailEnum.GUREUM.id -> {
+                    R.drawable.selector_square_check_coral_24dp
+                }
+                CharacterDetailEnum.BANGHYANG.id -> {
+                    R.drawable.selector_square_check_green_24dp
+                }
+                CharacterDetailEnum.MAEUM.id -> {
+                    R.drawable.selector_square_check_yellow_24dp
+                }
+                else -> {
+                    R.drawable.selector_square_check_purple_24dp
+                }
+            }
+        )
+
+    this.run {
+        buttonDrawable = null
+        setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
+    }
 }
