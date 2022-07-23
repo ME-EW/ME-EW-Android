fc31920366d08e5c88801e0aca222f1ede23461f
kangmin1012 <kangmin1012@naver.com>
2022-07-23 15:24:43 +0900

    Flow Builder 패턴

--- app/src/main/java/com/kangmin/meew/util/FlowApiBuilder.kt
+++ app/src/main/java/com/kangmin/meew/util/FlowApiBuilder.kt
@@ -1,8 +1,8 @@
 package com.kangmin.meew.util
 
-import android.os.Parcel
-import android.os.Parcelable
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import retrofit2.HttpException
 
 class FlowApi() {
     private var httpException: (suspend (errCode: Int) -> Unit)? = null
@@ -10,7 +10,11 @@ class FlowApi() {
     private var onSuccess: (suspend () -> Unit)? = null
 
 
-    inner class FlowBuilder<T>(flow: Flow<T>) {
+    inner class FlowBuilder<T>(private val flowItem: Flow<T>) {
+
+        fun onSuccess(l: suspend () -> Unit) {
+            onSuccess = l
+        }
 
         fun onHttpException(l: (suspend (errCode: Int) -> Unit)) {
             httpException = l
@@ -18,6 +22,18 @@ class FlowApi() {
 
         fun onEtcException(l: (suspend () -> Unit)) {
             etcException = l
+        }
+
+        suspend fun build() {
+            flowItem.catch {
+                if (it is HttpException) {
+                    httpException
+                } else {
+                    etcException
+                }
+            }.collect {
+                onSuccess
+            }
         }
     }
 }
